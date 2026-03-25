a948f50772fff8eed0b6f2b2494dcb5e2f9e5d23
Fistxszek <kacper.orzechowski.04@gmail.com>
2026-03-25 19:44:06 +0100

    almost done

--- app.py
+++ app.py
@@ -1,11 +1,80 @@
+from discord_webhook import DiscordWebhook, DiscordEmbed
 import feedparser
 import requests
 import time
 import json
 import os
+import re
 
 URL = 'http://www.skillshot.pl/jobs/feed/all'
 CACHE_FILES = 'feed_cache.json'
+
+# WEBHOOK_URL = 'https://discord.com/api/webhooks/1442169407962091672/G6_H8MDa9IyFr79oSdMERsyLUewatvTruQjGZbCIyrELVC0fFStyxMfcu9QQy38knHlW'
+WEBHOOK_URL = 'https://discord.com/api/webhooks/1486377780089454673/GhGot9Y7zi3Ndt32q6CfNdmmtHjieFsLRClvh44lqEIvne0iBPR-reqkc02tHLZwyAQ2'
+
+ADDITIONAL_INFO = ["Umowa o pracę", "Umowa o dzieło", "Umowa zlecenie", "B2B", "Część etatu", "Pół etatu", "Elastyczne godziny pracy", "Staż", "Praktyki"]
+
+EXPERIENCE = ["Junior", "Mid", "Senior"]
+
+ROLES = {
+    'Programowanie' : "<@&1442114495039410248>",
+    'Grafika i animacja' : "<@&1442116831539101737>",
+    'Design' : "<@&1442116910178238496>",
+    'Audio' : "<@&1442116955854078032>",
+    'Testowanie' : "<@&1442116983364522064>",
+    'Zarządzanie' : "<@&1442117004378116262>",
+    'Inne' : "<@&1442117029581553684>"
+}
+
+def get_first_image_from_html(html_content):
+    match = re.search(r'<img [^>]*src="([^"]+)"', html_content)
+    return match.group(1) if match else None
+
+def send_to_discord(entry):
+    embed = DiscordEmbed(
+        title=entry.title,
+        color="fa9405", 
+        url=entry.link
+    )
+
+    image = get_first_image_from_html(entry.summary)
+    if image:
+        embed.set_image(image)
+
+    embed.set_author(entry.author)
+    embed.set_thumbnail(url='https://www.skillshot.pl/gfx/skillshot-logo-square-256.jpg')
+    embed.add_embed_field(name="Rola", value=ROLES[entry.category], inline=True)
+
+    embed.add_embed_field(name="", value="", inline=False)
+    experienceKeyword = [word for word in EXPERIENCE if word.lower() in entry.title.lower()]
+
+    if experienceKeyword == "":
+        experienceKeyword = [word for word in EXPERIENCE if word.lower() in entry.summary.lower()]
+    if experienceKeyword:
+        embed.add_embed_field(name="Wymagane doświadczenie", value=experienceKeyword[0], inline=True)
+    else:
+        embed.add_embed_field(name="Wymagane doświadczenie", value="Nieznane", inline=True)
+
+    embed.add_embed_field(name="Lokalizacja", value=entry.get('location', 'Zdalna'), inline=True)
+    additional_info = [word for word in ADDITIONAL_INFO if word.lower() in entry.summary.lower()]
+
+    final_info = ""
+    for info in additional_info:
+        info = f"{info}"
+        final_info = f"{final_info + info} "
+    if final_info:
+        embed.add_embed_field(name="Dodatkowe informacje", value=final_info, inline=True)
+    
+    embed.add_embed_field(name="", value="", inline=False)
+
+    embed.set_timestamp()
+    if hasattr(entry, 'published_parsed'):
+        data_str = time.strftime('%d.%m.%Y %H:%M', entry.published_parsed)
+        embed.set_footer(text=f"Opublikowano na Skillshot.pl: {data_str}.\nGodzina powiadomienia")
+
+    webhook = DiscordWebhook(url=WEBHOOK_URL)
+    webhook.add_embed(embed)
+    webhook.execute()
 
 def load_cache():
     if os.path.exists(CACHE_FILES):
@@ -41,24 +110,12 @@ def getNewEntries():
         print(f"Feed Title: {feed.feed.get('title')}")
         print(f"Feed Description: {feed.feed.get('description')}")
 
-        entry = feed.entries[0]
-        keywords = ['junior', 'mid', 'senior', 'pracy']
-        content = entry.summary.lower()
-
-        found = [word for word in keywords if word in content]
-
-        if found:
-            print(f"Znalezino dopasowania: {found}")
+        # if found:
+        #     print(f"Znalezino dopasowania: {found}")
         # for entry in feed.entries:
-        #     print(f"\nTitle: {entry.title}")
-        #     print(f"Link: {entry.URL}")
-        #     print(f"Kategoria: {entry.category}")
-        #     print(f"Lokalizacja: {entry.location}")
-        #     print(f"ID: {entry.id}")
-        #     if hasattr(entry, 'published_parsed'):
-        #         readable_date = time.strftime('%d.%m.%Yr %H:%M', entry.published_parsed)
-        #         print(f"Date: {readable_date}")
+        #     send_to_discord(entry)
         
+        send_to_discord(feed.entries[0])  
         new_etag = response.headers.get('etag')
         new_modified = response.headers.get('last-modified')
         save_cache(new_etag, new_modified)
